b8ad03dc48c9c876071e2c813918a672ecee92b1
pi <pi@adoco>
2024-12-06 23:06:49 +0000

    Major optimizations for 2024-06-b
    Also add some other ergonomic structs that will very likely be re-used.

--- src/bin/year2024day06b.rs
+++ src/bin/year2024day06b.rs
@@ -1,6 +1,57 @@
-use std::collections::{HashMap, HashSet};
+use rustc_hash::FxHashSet;
 
 use aoc_2024_rs::*;
+
+#[derive(Debug, Hash, Eq, PartialEq, Copy, Clone)]
+struct Point2 {
+    x: i32,
+    y: i32,
+}
+
+impl Point2 {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y }
+    }
+
+    fn min() -> Self {
+        Self::new(0, 0)
+    }
+
+    fn max() -> Self {
+        Self::new(i32::MAX, i32::MAX)
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct BBox2 {
+    min: Point2,
+    max: Point2,
+}
+
+impl BBox2 {
+    #[allow(dead_code)]
+    fn new(min: Point2, max: Point2) -> Self {
+        BBox2 { min, max }
+    }
+
+    fn default() -> Self {
+        BBox2 {
+            min: Point2::max(),
+            max: Point2::min(),
+        }
+    }
+
+    fn update(&mut self, p: &Point2) {
+        self.min.x = self.min.x.min(p.x);
+        self.min.y = self.min.y.min(p.y);
+        self.max.x = self.max.x.max(p.x);
+        self.max.y = self.max.y.max(p.y);
+    }
+
+    fn contains(&self, p: &Point2) -> bool {
+        p.x >= self.min.x && p.x <= self.max.x && p.y >= self.min.y && p.y <= self.max.y
+    }
+}
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum Direction {
@@ -20,12 +71,12 @@ impl Direction {
         }
     }
 
-    fn step(&self, x: i32, y: i32) -> (i32, i32) {
+    fn step(&self, p: &Point2) -> Point2 {
         match self {
-            Self::North => (x, y - 1),
-            Self::East => (x + 1, y),
-            Self::South => (x, y + 1),
-            Self::West => (x - 1, y),
+            Self::North => Point2::new(p.x, p.y - 1),
+            Self::East => Point2::new(p.x + 1, p.y),
+            Self::South => Point2::new(p.x, p.y + 1),
+            Self::West => Point2::new(p.x - 1, p.y),
         }
     }
 }
@@ -66,19 +117,31 @@ impl std::fmt::Display for Tile {
 
 #[derive(Debug, Clone)]
 struct State {
-    grid: HashMap<(i32, i32), Tile>,
-    bbox: ((i32, i32), (i32, i32)),
-    guard_at: (i32, i32),
+    obstructions: FxHashSet<Point2>,
+    bbox: BBox2,
+    guard_at: Point2,
     guard_face: Direction,
 }
 
 impl State {
     fn new() -> Self {
         Self {
-            grid: HashMap::new(),
-            bbox: ((i32::MAX, i32::MAX), (0i32, 0i32)),
-            guard_at: (0, 0),
+            obstructions: FxHashSet::default(),
+            bbox: BBox2::default(),
+            guard_at: Point2::new(0, 0),
             guard_face: Direction::North,
+        }
+    }
+
+    fn get(&self, p: &Point2) -> Option<Tile> {
+        if !self.bbox.contains(p) {
+            None
+        } else if self.obstructions.contains(&p) {
+            Some(Tile::Obstruction)
+        } else if *p == self.guard_at {
+            Some(Tile::Guard(self.guard_face))
+        } else {
+            Some(Tile::Open)
         }
     }
 }
@@ -91,21 +154,18 @@ fn parse_input(input: String) -> State {
             continue;
         }
         for (x, col) in line.trim().char_indices() {
-            let t = match Tile::from(col) {
+            let p = Point2::new(x.try_into().unwrap(), y.try_into().unwrap());
+            match Tile::from(col) {
                 Tile::Guard(d) => {
-                    state.guard_at = (x.try_into().unwrap(), y.try_into().unwrap());
+                    state.guard_at = p;
                     state.guard_face = d;
-                    Tile::Open
                 }
-                other => other,
+                Tile::Obstruction => {
+                    state.obstructions.insert(p);
+                }
+                Tile::Open => {}
             };
-            state
-                .grid
-                .insert((x.try_into().unwrap(), y.try_into().unwrap()), t);
-            state.bbox.0 .0 = state.bbox.0 .0.min(x.try_into().unwrap());
-            state.bbox.0 .1 = state.bbox.0 .1.min(y.try_into().unwrap());
-            state.bbox.1 .0 = state.bbox.1 .0.max(x.try_into().unwrap());
-            state.bbox.1 .1 = state.bbox.1 .1.max(y.try_into().unwrap());
+            state.bbox.update(&p);
         }
     }
 
@@ -114,44 +174,35 @@ fn parse_input(input: String) -> State {
 
 #[allow(dead_code)]
 fn pprint_grid(state: &State) {
-    for y in state.bbox.0 .1..=state.bbox.1 .1 {
-        for x in state.bbox.0 .0..=state.bbox.1 .0 {
-            let t = if (x, y) == state.guard_at {
-                &Tile::Guard(state.guard_face)
-            } else {
-                state.grid.get(&(x, y)).unwrap()
-            };
-            print!("{}", t);
+    for y in state.bbox.min.y..=state.bbox.max.y {
+        for x in state.bbox.min.x..=state.bbox.max.x {
+            print!("{}", state.get(&Point2::new(x, y)).unwrap());
         }
         println!();
     }
 }
 
-fn patrol(state: &State) -> (HashSet<((i32, i32), Direction)>, bool) {
+fn patrol(state: &State) -> (FxHashSet<(Point2, Direction)>, bool) {
     let mut guard_at = state.guard_at;
     let mut guard_face = state.guard_face;
 
-    let mut visited =
-        HashSet::with_capacity((state.bbox.1 .0 * state.bbox.1 .1).try_into().unwrap());
+    let mut visited = FxHashSet::default();
+    visited.reserve((state.bbox.max.x * state.bbox.max.y).try_into().unwrap());
     visited.insert((guard_at, guard_face));
 
     loop {
-        let next_at = guard_face.step(guard_at.0, guard_at.1);
+        let next_at = guard_face.step(&guard_at);
 
-        match state.grid.get(&next_at) {
-            Some(t) => match t {
-                Tile::Open => {
-                    guard_at = next_at;
-                    let key = (guard_at, guard_face);
-                    if !visited.insert(key) {
-                        return (visited, true);
-                    }
+        match state.get(&next_at) {
+            Some(Tile::Obstruction) => {
+                guard_face = guard_face.rotate_right();
+            }
+            Some(Tile::Open | Tile::Guard(_)) => {
+                guard_at = next_at;
+                if !visited.insert((guard_at, guard_face)) {
+                    return (visited, true);
                 }
-                Tile::Obstruction => {
-                    guard_face = guard_face.rotate_right();
-                }
-                _ => {}
-            },
+            }
             None => break,
         }
     }
@@ -159,23 +210,24 @@ fn patrol(state: &State) -> (HashSet<((i32, i32), Direction)>, bool) {
     (visited, false)
 }
 
-// TODO: Could probably use "vectors" instead of individual steps.
+// TODO: Could probably use "vectors" instead of individual steps?
+// Or maybe pre-compute jumps?
 
-fn get_looping_obstacles(state: &State) -> HashSet<(i32, i32)> {
+fn get_looping_obstacles(state: &State) -> FxHashSet<Point2> {
     let mut current = state.clone();
     let (visited, _) = patrol(state);
 
-    let mut looping_obstacles = HashSet::new();
+    let mut looping_obstacles = FxHashSet::default();
 
-    for ((x, y), _) in visited {
-        if (x, y) == state.guard_at {
+    for (p, _) in visited {
+        if p == state.guard_at {
             continue;
         }
-        let old_t = current.grid.insert((x, y), Tile::Obstruction).unwrap();
+        current.obstructions.insert(p);
         let (_, looped) = patrol(&current);
-        current.grid.insert((x, y), old_t);
+        current.obstructions.remove(&p);
         if looped {
-            looping_obstacles.insert((x, y));
+            looping_obstacles.insert(p);
         }
     }
 
@@ -217,24 +269,40 @@ mod tests {
 
         pprint_grid(&parsed);
 
-        assert_eq!(&Tile::Open, parsed.grid.get(&(0, 0)).unwrap());
-        assert_eq!(&Tile::Obstruction, parsed.grid.get(&(4, 0)).unwrap());
-        assert_eq!(&Tile::Open, parsed.grid.get(&(4, 6)).unwrap());
-        assert_eq!(&Tile::Obstruction, parsed.grid.get(&(6, 9)).unwrap());
+        assert_eq!(Some(Tile::Open), parsed.get(&Point2::new(0, 0)));
+        assert_eq!(Some(Tile::Obstruction), parsed.get(&Point2::new(4, 0)));
+        assert_eq!(
+            Some(Tile::Guard(Direction::North)),
+            parsed.get(&Point2::new(4, 6))
+        );
+        assert_eq!(Some(Tile::Obstruction), parsed.get(&Point2::new(6, 9)));
 
-        assert_eq!((4, 6), parsed.guard_at);
+        assert_eq!(Point2::new(4, 6), parsed.guard_at);
         assert_eq!(Direction::North, parsed.guard_face);
 
-        assert_eq!(((0, 0,), (9, 9)), parsed.bbox);
+        assert_eq!(
+            BBox2::new(Point2::new(0, 0), Point2::new(9, 9)),
+            parsed.bbox
+        );
 
-        let expected_loops = HashSet::from([(3, 6), (6, 7), (7, 7), (1, 8), (3, 8), (7, 9)]);
+        let expected_loops = vec![
+            Point2::new(3, 6),
+            Point2::new(6, 7),
+            Point2::new(7, 7),
+            Point2::new(1, 8),
+            Point2::new(3, 8),
+            Point2::new(7, 9),
+        ]
+        .iter()
+        .cloned()
+        .collect::<FxHashSet<Point2>>();
         let actual_loops = get_looping_obstacles(&parsed);
         assert_eq!(expected_loops.len(), actual_loops.len());
         assert_eq!(expected_loops, actual_loops);
 
         assert_eq!(6, solve(&parsed));
 
-        assert_eq!((4, 6), parsed.guard_at);
+        assert_eq!(Point2::new(4, 6), parsed.guard_at);
         assert_eq!(Direction::North, parsed.guard_face);
     }
 }
