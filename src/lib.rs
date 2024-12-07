242ab01d7d870d934e4a058f85089434de581380
pi <pi@adoco>
2024-12-07 14:13:27 +0000

    Support multiple inputs with AOC_PROFILE env var
    Useful if, for example, you have multiple AOC accounts, for whatever
    reason. Who knows? The world is crazy out there sometimes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 pub fn load_input(year: u16, day: u8) -> String {
-    let filename = format!("{}-{:02}-input.txt", year, day);
+    let profile = std::env::var("AOC_PROFILE").unwrap_or("default".to_string());
+    let filename = format!("{}-{}-{:02}-input.txt", profile, year, day);
     let filepath = std::path::PathBuf::from(format!("../utils/.cache/{}", filename));
-    std::fs::read_to_string(filepath).expect("Could not read input file.")
+    std::fs::read_to_string(filepath.clone()).unwrap_or_else(|_| {
+        panic!(
+            "{}",
+            format!("Could not read input file: {:?}", filepath).to_string()
+        )
+    })
 }
